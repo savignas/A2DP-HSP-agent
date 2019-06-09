0b4c615b8074fd4a028c3899df233d579b73f887
Ignas Savickas <i.savickas@outlook.com>
2019-06-09 18:09:50 +0300

    Code refactor

--- usr/local/bin/a2dp-hsp-agent.py
+++ usr/local/bin/a2dp-hsp-agent.py
@@ -18,21 +18,68 @@ except ImportError:
 AGENT_INTERFACE = "org.bluez.Agent1"
 HANDS_FREE_INTERFACE = "org.ofono.Handsfree"
 AGENT_PATH = "/test/agent"
-DEVICES_FILE_PATH = "/var/local/devices.txt"
+FILE_DIR_PATH = "/var/local/"
+DEVICES_FILE_PATH = FILE_DIR_PATH + "devices.txt"
 A2DP_UUID = "0000110d-0000-1000-8000-00805f9b34fb"
 HFP_UUID = "0000111e-0000-1000-8000-00805f9b34fb"
 
 bus = None
 connected = True
+modem_path = None
 a2dp_connected = False
 hfp_connected = False
 inband_ringing = False
 incomming_call = None
 
-def set_trusted(path):
-    props = dbus.Interface(bus.get_object("org.bluez", path),
-                            "org.freedesktop.DBus.Properties")
-    props.Set("org.bluez.Device1", "Trusted", True)
+class Rejected(dbus.DBusException):
+	_dbus_error_name = "org.bluez.Error.Rejected"
+
+class Agent(dbus.service.Object):
+	exit_on_release = True
+
+	def set_exit_on_release(self, exit_on_release):
+		self.exit_on_release = exit_on_release
+
+	@dbus.service.method(AGENT_INTERFACE,
+					in_signature="", out_signature="")
+	def Release(self):
+		print("Release")
+		if self.exit_on_release:
+			mainloop.quit()
+
+	@dbus.service.method(AGENT_INTERFACE,
+					in_signature="os", out_signature="")
+	def AuthorizeService(self, device, uuid):
+		global connected
+		print("AuthorizeService (%s, %s)" % (device, uuid))
+		if uuid == A2DP_UUID and not a2dp_connected:
+			print("Authorized A2DP Service")
+			add_device(device)
+			return
+		elif uuid == HFP_UUID and not hfp_connected:
+			print("Authorized HFP Service")
+			add_device(device)
+			return
+		print("Rejecting Service")
+		raise Rejected("Connection rejected")
+
+def add_device(new_device):
+    try:
+        file_read = open(DEVICES_FILE_PATH, "r")
+        devices = file_read.read().splitlines()
+        file_read.close()
+        if new_device in devices: devices.remove(new_device)
+        else: print("New device added (%s)" % new_device)
+        devices.insert(0, new_device)
+        if (len(devices) > 6): del devices[6:len(devices)]
+        file_write = open(DEVICES_FILE_PATH, "w")
+        file_write.write('\n'.join(devices) + '\n')
+        file_write.close()
+    except Exception:
+        file = open(DEVICES_FILE_PATH, "w")
+        file.write(new_device + '\n')
+        print("New device added (%s)" % new_device)
+        file.close()
 
 def dev_connect(path):
     dev = dbus.Interface(bus.get_object("org.bluez", path),
@@ -59,50 +106,32 @@ def connect_to_dev():
         connected = False
         file.close()
     except Exception:
-        print("No recent devices to connect...")
+        print("No recent devices to connect. Waiting for connection...")
         return
-
-def add_device(new_device):
-    try:
-        file_read = open(DEVICES_FILE_PATH, "r")
-        devices = file_read.read().splitlines()
-        file_read.close()
-        if new_device in devices: devices.remove(new_device)
-        else: print("New device added (%s)" % new_device)
-        devices.insert(0, new_device)
-        if (len(devices) > 6): del devices[6:len(devices)]
-        file_write = open(DEVICES_FILE_PATH, "w")
-        file_write.write('\n'.join(devices) + '\n')
-        file_write.close()
-    except Exception:
-        file = open(DEVICES_FILE_PATH, "w")
-        file.write(new_device + '\n')
-        print("New device added (%s)" % new_device)
-        file.close()
 
 def play_connected_sound():
 	global connected
 	if not connected:
 		connected = True
-		subprocess.call(["aplay", "/var/local/connected.wav"])
+		subprocess.call(["aplay", FILE_DIR_PATH + "connected.wav"])
 
 def play_disconnect_sound():
 	if not (a2dp_connected or hfp_connected):
 		global connected
 		connected = False
-		subprocess.call(["aplay", "/var/local/disconnected.wav"])
+		subprocess.call(["aplay", FILE_DIR_PATH + "disconnected.wav"])
 
 def play_battery_sound():
-	subprocess.call(["aplay", "/var/local/battery.wav"])
+	subprocess.call(["aplay", FILE_DIR_PATH + "battery.wav"])
 
 def player_changed(interface, properties, invalidated):
 	if interface == "org.bluez.MediaControl1" and 'Connected' in properties:
 		global a2dp_connected
-		if properties['Connected']:
+		if properties['Connected'] and not a2dp_connected:
 			print("A2DP Connected")
 			a2dp_connected = True
 			play_connected_sound()
-		else:
+		elif not properties['Connected'] and a2dp_connected:
 			print("A2DP Disconnected")
 			a2dp_connected = False
 			play_disconnect_sound()
@@ -116,7 +145,7 @@ def hands_free_changed(property, value):
 def modem_changed(property, value):
 	if property == "Powered":
 		global hfp_connected, incomming_call
-		if value:
+		if value and not hfp_connected:
 			print("HSP Connected")
 			hfp_connected = True
 			play_connected_sound()
@@ -125,13 +154,16 @@ def modem_changed(property, value):
 			modems = manager.GetModems()
 			for modem in modems:
 				if modem[1]["Powered"]:
-					get_hfp_properties(modem[0])
+					modem_path = modem[0]
+					get_hfp_properties(modem_path)
 					break
-		else:
+		elif not value and hfp_connected:
 			print("HSP Disconnected")
 			hfp_connected = False
-			incomming_call.terminate()
-			incomming_call = None
+			if incomming_call:
+				incomming_call.terminate()
+				incomming_call = None
+			modem_path = None
 			play_disconnect_sound()
 
 def voice_call_changed(property, value):
@@ -145,7 +177,7 @@ def voice_call_added(path, properties):
 	global incomming_call
 	print("Call added (%s):" % path, properties['State'])
 	if properties['State'] == "incoming" and incomming_call is None and not inband_ringing:
-		incomming_call = subprocess.Popen(["aplay", "/var/local/calling.wav"])
+		incomming_call = subprocess.Popen(["aplay", FILE_DIR_PATH + "calling.wav"])
 
 def voice_call_removed(path):
 	global incomming_call
@@ -175,101 +207,26 @@ def add_signal_receivers():
 		signal_name = "PropertyChanged")
 
 def get_hfp_properties(path):
+	global inband_ringing
 	hfp = dbus.Interface(bus.get_object("org.ofono", path),
                             HANDS_FREE_INTERFACE)
 	properties = hfp.GetProperties()
 	battery_level = int(properties['BatteryChargeLevel'])
-	global inband_ringing
 	inband_ringing = properties['InbandRinging']
 	print("In-band ringing:", inband_ringing)
 	print("Current battery level:", battery_level)
 	if battery_level == 0: play_battery_sound()
 
-class Rejected(dbus.DBusException):
-	_dbus_error_name = "org.bluez.Error.Rejected"
-
-class Agent(dbus.service.Object):
-	exit_on_release = True
-
-	def set_exit_on_release(self, exit_on_release):
-		self.exit_on_release = exit_on_release
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="", out_signature="")
-	def Release(self):
-		print("Release")
-		if self.exit_on_release:
-			mainloop.quit()
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="os", out_signature="")
-	def AuthorizeService(self, device, uuid):
-		global connected
-		print("AuthorizeService (%s, %s)" % (device, uuid))
-                if uuid == A2DP_UUID and not a2dp_connected:
-                    print("Authorized A2DP Service")
-                    add_device(device)
-                    return
-                elif uuid == HFP_UUID and not hfp_connected:
-                    print("Authorized HFP Service")
-                    add_device(device)
-                    return
-                print("Rejecting Service")
-		raise Rejected("Connection rejected")
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="o", out_signature="s")
-	def RequestPinCode(self, device):
-		print("RequestPinCode (%s)" % (device))
-		set_trusted(device)
-		return "0000"
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="o", out_signature="u")
-	def RequestPasskey(self, device):
-		print("RequestPasskey (%s)" % (device))
-		set_trusted(device)
-		return dbus.UInt32("password")
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="ouq", out_signature="")
-	def DisplayPasskey(self, device, passkey, entered):
-		print("DisplayPasskey (%s, %06u entered %u)" %
-						(device, passkey, entered))
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="os", out_signature="")
-	def DisplayPinCode(self, device, pincode):
-		print("DisplayPinCode (%s, %s)" % (device, pincode))
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="ou", out_signature="")
-	def RequestConfirmation(self, device, passkey):
-		print("RequestConfirmation (%s, %06d)" % (device, passkey))
-		set_trusted(device)
-		return
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="o", out_signature="")
-	def RequestAuthorization(self, device):
-		print("RequestAuthorization (%s)" % (device))
-		raise Rejected("Pairing rejected")
-
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="", out_signature="")
-	def Cancel(self):
-		print("Cancel")
-
 if __name__ == '__main__':
-	subprocess.call(["aplay", "/var/local/bluetooth_mode.wav"])
-	
+	subprocess.call(["aplay", FILE_DIR_PATH + "bluetooth_mode.wav"])
+
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
 	bus = dbus.SystemBus()
 
 	agent = Agent(bus, AGENT_PATH)
 
-	obj = bus.get_object("org.bluez", "/org/bluez");
+	obj = bus.get_object("org.bluez", "/org/bluez")
 	manager = dbus.Interface(obj, "org.bluez.AgentManager1")
 	manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput")
 
@@ -278,9 +235,9 @@ if __name__ == '__main__':
 	manager.RequestDefaultAgent(AGENT_PATH)
 
 	mainloop = GObject.MainLoop()
-	
+
 	add_signal_receivers()
 	connect_to_dev()
-	
-	
+
+
 	mainloop.run()
